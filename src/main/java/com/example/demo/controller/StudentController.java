be2984c589603117fdf50a178e94316285edef32
bala-amypo <bala@amypo.in>
2025-12-17 11:43:50 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,5 +1,4 @@
 package com.example.demo.controller;
-
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,9 +7,7 @@ import com.example.demo.service.StudentService;
 import com.example.demo.entity.StudentEntity;
 @RestController
 public class StudentController{
-
-    @Autowired StudentService ser;
-
+    @Autowired StudentService ser;  
     @PostMapping("/post")
     public StudentEntity sendData(@RequestBody StudentEntity stu){
         return ser.postdata(stu);//to ser calling the service layer
