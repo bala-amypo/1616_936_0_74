bfc8c76e91a9d766698645ee330c33be628c4a6e
bala-amypo <bala@amypo.in>
2025-12-17 09:16:43 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
@@ -3,10 +3,10 @@ import org.springframework.stereotype.Service;
 import com.example.demo.service.StudentService;
 @Service
 public class StudentServiceImpl implements StudentService{
-   @Autowired StudentRepository student;//
-   //save(), findall(),findById(),deleteById(),existsById();
+   @Autowired StudentRepository student;//to access the methods in diff file
+   //save(), findAll(),findById(),deleteById(),existsById();
    public StudentEntity postdata(StudentEntity stu){
-    
+    return student.save(stu); //stu->userinput save->insert and update
    }
 
 }
