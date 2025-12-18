b925e64c13287f702839ea75b3aa8d78988b2da9
bala-amypo <bala@amypo.in>
2025-12-18 08:29:34 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.demo.service.Impl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.service.ValidationService;
+import com.example.demo.repository.ValidatRepository;
+import com.example.demo.entity.StudentEntity;
+import java.util.List;
+@Service
+public class StudentServiceImpl implements StudentService{
+   @Autowired StudentRepository student;//  acces the method in diff files
+   //save();
+   //findall();
+   //findById();
+   //deleteById();
+   //existsById();
+   @Override
+    public StudentEntity postdata(StudentEntity stu){
+        return student.save(stu);
+    }
+    @Override
+    public List<StudentEntity> getdata(){
+        return student.findAll();
+    }
+    @Override
+    public String deldata(Integer id){
+         student.deleteById(id);
+         return "Removed successfully";
+    }
+    @Override
+    public StudentEntity getbydata(Integer id){
+        return student.findById(id).orElse(null);    
+    }
+    @Override
+    public StudentEntity putdata(Integer id, StudentEntity std){
+        if (student.existsById(id)){// checks for the existence of id(true)
+            std.setId(id);// setting the id
+            return student.save(std);//replacing the id
+        }
+        else{
+            return null;
+        }
+    }
+}
