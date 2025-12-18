550866511b242f7044550f92ff8e21b2c6220b11
bala-amypo <bala@amypo.in>
2025-12-18 08:49:24 +0000

    User commit

--- src/main/java/com/example/demo/service/ValidationService.java
+++ src/main/java/com/example/demo/service/ValidationService.java
@@ -3,9 +3,9 @@ import com.example.demo.entity.ValidationEntity;
 import java.util.List;
 
 public interface ValidationService{
-    ValidationEntity postdata(ValidationEntity stu);
-    List<ValidationEntity> getdata();
-    String deldata(Long id);
-    ValidationEntity getbydata(Long id);
-    ValidationEntity putdata(Long id, ValidationEntity std);
+    ValidationEntity postdata2(ValidationEntity stu);
+    // List<ValidationEntity> getdata();
+    // String deldata(Long id);
+    // ValidationEntity getbydata(Long id);
+    // ValidationEntity putdata(Long id, ValidationEntity std);
 }
