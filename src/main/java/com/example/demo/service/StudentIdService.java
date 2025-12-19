d2fdca7b1d4ce01165089438ba50773a5c5860c9
bala-amypo <bala@amypo.in>
2025-12-19 10:36:16 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/StudentIdService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+import com.example.demo.entity.StudentService;
+// import java.util.List;
+
+public interface TimeStampService{
+    TimeStamp postdata3(TimeStamp stu2);
+    // List<ValidationEntity> getdata();
+    // String deldata(Long id);
+    // ValidationEntity getbydata(Long id);
+    // ValidationEntity putdata(Long id, ValidationEntity std);
+}
