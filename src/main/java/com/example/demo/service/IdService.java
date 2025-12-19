2322e14b2c8aec6bd39660f7bae24225225e4e61
bala-amypo <bala@amypo.in>
2025-12-19 10:40:34 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/IdService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+import com.example.demo.entity.IdEntity;
+// import java.util.List;
+
+public interface IdService{
+    IdEntity postdata5(IdEntity stu5);
+    // List<ValidationEntity> getdata();
+    // String deldata(Long id);
+    // ValidationEntity getbydata(Long id);
+    // ValidationEntity putdata(Long id, ValidationEntity std);
+}
