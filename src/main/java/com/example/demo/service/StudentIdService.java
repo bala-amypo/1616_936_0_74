949fdc59db133e5f66b9809a1e24d97b32224df4
bala-amypo <bala@amypo.in>
2025-12-19 10:37:07 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentIdService.java
+++ src/main/java/com/example/demo/service/StudentIdService.java
@@ -1,9 +1,9 @@
 package com.example.demo.service;
-import com.example.demo.entity.StudentServ;
+import com.example.demo.entity.StudentIdEntity;
 // import java.util.List;
 
-public interface TimeStampService{
-    TimeStamp postdata3(TimeStamp stu2);
+public interface StudentIdService{
+    StudentIdEntity postdata4(StudentIdEntity stu4);
     // List<ValidationEntity> getdata();
     // String deldata(Long id);
     // ValidationEntity getbydata(Long id);
