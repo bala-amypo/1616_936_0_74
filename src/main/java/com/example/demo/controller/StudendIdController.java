89fc9374b108ae7f7b89abac489b08006b997e98
bala-amypo <bala@amypo.in>
2025-12-19 10:42:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/StudendIdController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+// import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.service.TimeStampService;
+import com.example.demo.entity.TimeStamp;
+import jakarta.validation.Valid;
+@RestController
+public class TimeStampController{
+    @Autowired TimeStampService ser;  
+    @PostMapping("/postt")
+    public TimeStamp sendData3(@Valid @RequestBody TimeStamp stu2){
+        return ser.postdata3(stu2);//to ser calling the service layer
+    }
+    // @GetMapping("/get2")
+    // public List<ValidationEntity> getData(){
+    //     return ser.getdata();
+    // }
+    // @DeleteMapping("/del2/{id}")
+    // public String delData(@PathVariable Long id){
+    //     return ser.deldata(id);
+    // }
+    // @GetMapping("/putgett2/{id}")
+    // public ValidationEntity GetByData(@PathVariable Long id){
+    //     return ser.getbydata(id);
+    // }
+    // @PutMapping("/put2/{id}")
+    // public ValidationEntity putData(@PathVariable Long id, @RequestBody ValidationEntity std){
+    //     return ser.putdata(id,std); 
+    // }
+}
