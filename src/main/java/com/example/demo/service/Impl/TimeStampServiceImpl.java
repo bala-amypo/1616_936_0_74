020371b6369bab55e5f4252adb1f79e99ef15e77
bala-amypo <bala@amypo.in>
2025-12-19 07:48:16 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/TimeStampServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TimeStampServiceImpl.java
@@ -1,10 +1,10 @@
 package com.example.demo.service.Impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.demo.service.ValidationService;
+import com.example.demo.service.TimeStampService;
 import com.example.demo.repository.ValidationRepository;
 // import com.example.demo.entity.ValidationEntity;
-import com.example.demo.exception.ValidationException;
+// import com.example.demo.exception.ValidationException;
 
 import java.util.List;
 @Service
@@ -16,7 +16,7 @@ public class ValidationServiceImpl implements ValidationService{
    //deleteById();
    //existsById();
    @Override
-    public ValidationEntity postdata2(ValidationEntity stu){
+    public TimeStamp postdata2(TimeStamp stu){
         return valid.save(stu);
     }
     // @Override
