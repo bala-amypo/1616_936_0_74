b6356992da7b48e9ab36a215163d3f44700290ac
bala-amypo <bala@amypo.in>
2025-12-19 05:04:02 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Service;
 import com.example.demo.service.ValidationService;
 import com.example.demo.repository.ValidationRepository;
 import com.example.demo.entity.ValidationEntity;
-import java.util.List;
 import com.example.demo.exception.ValidationException;
+
+import java.util.List;
 @Service
 public class ValidationServiceImpl implements ValidationService{
    @Autowired ValidationRepository valid;//  acces the method in diff files
